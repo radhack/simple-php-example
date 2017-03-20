e2819146970c10b21d8f7815c595ac62697f19c8
alexgriffen <alexgriffen@alexgriffensmbp>
2017-03-20 12:46:33 -0700

    update email template AppendedSignaturePage_email.php and reviewdocument .php

--- reviewdocument.php
+++ reviewdocument.php
@@ -4,6 +4,8 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Review Document and Sign</title>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="//s3.amazonaws.com/cdn.hellosign.com/public/js/hellosign-embedded.LATEST.min.js"></script>
         <link rel="stylesheet" type="text/css" href="newcss.css" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" href="/favicon-32x32.png"/>
